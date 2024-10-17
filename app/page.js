32f80891c432f60544a35a33cbf049d849259cb0
shamimranapro85 <shamimranaprofessional@gmail.com>
2024-10-17 16:04:07 +0600

    all code deploy for commit

--- app/page.js
+++ app/page.js
@@ -10,24 +10,20 @@ import { useEffect, useState } from "react";
 export default function Home() {
   const [data, setdata] = useState("");
 
-  // useEffect(() => {
-  //   (async () => {
-  //     let responsed = await axios.post(`${backendURL}/user/login`, {
-  //       token: localStorage.getItem("islogin"),
-  //     });
-  //     setdata(responsed.data.payLoad);
+  useEffect(() => {
+    (async () => {
+      let responsed = await axios.post(`${backendURL}/user/login`, {
+        token: localStorage.getItem("islogin"),
+      });
+      setdata(responsed.data.payLoad);
 
-  //   })();
-  // }, []);
+    })();
+  }, []);
 
   return (
     <>
-      {/* {data ? <DashBoard  name={data.name} email={data.email}/> : <Login />} */}
-      <h1 className="text-red text-center text-6xl capitalize font-bold h-screen flex justify-center items-center">
-        shamim <br/>
-        shanto <br/>
-        Forhad  
-      </h1>
+      {data ? <DashBoard  name={data.name} email={data.email}/> : <Login />}
+    
 
     </>
   );
