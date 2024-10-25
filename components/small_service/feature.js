b5b4252713d011f31fa1e73d8b675528e55c48ea
shamimranapro85 <shamimranaprofessional@gmail.com>
2024-10-25 23:09:30 +0600

    add online backend server

--- components/small_service/feature.js
+++ components/small_service/feature.js
@@ -4,7 +4,7 @@
 let Loading_div = (
   <div class="w-2 h-2 mr-2 border-2 border-t-transparent border-green-500 rounded-full animate-spin"></div>
 );
-const backendURL = "http://localhost:3001";
-// const backendURL = "https://full-stack-backend-ten.vercel.app";
+// const backendURL = "http://localhost:3001";
+const backendURL = "https://full-stack-backend-ten.vercel.app";
 
 module.exports = { Loading_div, backendURL};
